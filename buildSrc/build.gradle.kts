df6bfa79b53833e65826436e5a541c709183fde3
Eric Goodhall <eric_g@fastmail.com>
2025-10-09 10:09:51 -0400

    Update buildSrc buildfile

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,7 +1,4 @@
 plugins {
-  // The Kotlin DSL plugin provides a convenient way to develop convention plugins.
-  // Convention plugins are located in `src/main/kotlin`, with the file extension `.gradle.kts`,
-  // and are applied in the project's `build.gradle.kts` files as required.
   `kotlin-dsl`
   alias(libs.plugins.spotless)
 }
@@ -11,7 +8,6 @@ kotlin {
 }
 
 dependencies {
-  // Add a dependency on the Kotlin Gradle plugin, so that convention plugins can apply it.
   implementation(libs.kotlin.gradle.plugin)
   implementation(plugin(libs.plugins.spotless))
 }
